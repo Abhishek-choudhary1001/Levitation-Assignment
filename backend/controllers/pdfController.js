f22ed3251bed22b1bc880d9876b20816d1d7d485
Abhishek-choudhary1001 <abhishekcool301@gmail.com>
2025-08-21 22:23:37 +0530

    pdf generation

--- backend/controllers/pdfController.js
+++ backend/controllers/pdfController.js
@@ -10,7 +10,7 @@ export const generateInvoice = async (req, res) => {
 
     const browser = await puppeteer.launch({
       headless: true,
-      args: ["--no-sandbox", "--disable-setuid-sandbox"],
+      args: ["--no-sandbox", "--disable-setuid-sandbox", "--disable-dev-shm-usage"],
     });
     const page = await browser.newPage();
 
